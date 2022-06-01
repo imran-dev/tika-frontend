5c557e02568e267f529657f0317149801ddbd520
IMRAN HOSSAIN <imran.dev.bd@gmail.com>
2022-06-02 03:31:20 +0600

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1 v-if="apiValue" class="font-bold text-3xl">Check: {{apiValue}}</h1>
+    <button @click.prevent="testBackendRequest">Request API</button>
+    <Tutorial/>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  data(){
+    return {
+      apiValue: ''
+    }
+  },
+  mounted() {
+    // this.testBackendRequest();
+  },
+  methods:{
+    testBackendRequest(){
+      this.$axios.get('/test').then(res => {
+        this.apiValue = res.data;
+      });
+    }
+  }
 }
 </script>
